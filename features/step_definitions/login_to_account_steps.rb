a488ff49215513d36a9853cea3c58eac72e5a10e
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-09-19 14:24:41 +0200

    Create FactoryBot first test green

--- features/step_definitions/login_to_account_steps.rb
+++ features/step_definitions/login_to_account_steps.rb
@@ -1,3 +1,21 @@
 Given("User has an existing account") do |table|
-    
+    table.hashes.each do |user|
+        FactoryBot.create(:user)
+    end
+end
+
+Given("I am on the landing page") do
+    visit root_path
+end
+  
+When("I click on {string}") do |string|
+    pending 
+end
+  
+When("I fill in {string} with {string}") do |string, string2|
+    pending 
+end
+  
+Then("I should see message {string}") do |string|
+    pending 
 end
