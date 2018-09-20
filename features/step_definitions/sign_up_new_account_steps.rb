18eb8286f743ce1f69fa0b26d443f868fa8682e6
rhon <rhoneil.gabriel@me.com>
2018-09-20 13:32:56 +0200

    Test is now saving credentials, last test failing

--- features/step_definitions/sign_up_new_account_steps.rb
+++ features/step_definitions/sign_up_new_account_steps.rb
@@ -1,6 +1,6 @@
 Given("User provide the following credentials") do |table|
     table.hashes.each do |user|
-        FactoryBot.create(:user, user)
+        FactoryBot.create(:user)
       end
   end
 
@@ -8,17 +8,17 @@ Given("I am on the index page") do
     visit root_path
 end
     
-  When("I click on signup {string}") do |button2|
+When("I click on signup {string}") do |button2|
     click_on button2 
-  end
+end
   
-  And("I fill in {string} with {string}") do |string1, string2|
+And("I fill in {string} with {string}") do |string1, string2|
     fill_in(string1, with: string2)
-  end
+end
     
-  When("i click on {string}") do |link|
+When("i click on {string}") do |link|
     click_on link
-  end
+end
 
 Then("I should see a message {string}") do |string|
     expect(page).to have_content message
