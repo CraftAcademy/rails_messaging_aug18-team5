02c5fe813b61d77627c06804dbf341ed1f67f436
rhon <rhoneil.gabriel@me.com>
2018-09-20 10:47:36 +0200

    Added spec validations and factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe User, type: :model do
     describe 'DB table' do
         it { is_expected.to have_db_column :name }
     end
+
+    describe 'Validations' do
+        it { is_expected.to validate_presence_of :name}
+        it { is_expected.to validate_presence_of :email}
+        it { is_expected.to validate_presence_of :password}
+    end
+
+    describe 'Factory' do
+        it 'should have valid Factory' do
+          expect(FactoryBot.create(:user)).to be_valid 
+        end
+    end
 end
